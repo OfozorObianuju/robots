3207a2a6314cf214c3ec0aca35bbe46415304966
ofozorObianuju <ofozormerit1@gmail.com>
2019-09-25 16:04:42 +0100

    created card component

---
+++ src/robots.js
@@ -0,0 +1,29 @@
+export const robots = [
+{
+    id:1,
+    name: 'Leanne Graham',
+    username: 'Bret',
+    email: 'Sincer@april.biz'
+},
+{
+
+    id: 2,
+    name: 'Ervin Howell',
+    username: 'Antonette',
+    email: 'Shanna@melissa.tv'
+},
+{
+
+    id: 3,
+    name: 'clementine Bauch',
+    username: 'Samantha',
+    email: 'Nathan@yesenia.net'
+},
+{
+
+    id: 4,
+    name: 'patricia Lebsack',
+    username: 'Karianne',
+    email: 'Julianne.oconner@Kory.org'
+},
+];
