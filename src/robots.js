21fe514b05a125e93d664b0639229a1707d151ed
ofozorObianuju <ofozormerit1@gmail.com>
2019-10-07 15:38:13 +0100

    Created searchbox

--- src/robots.js
+++ src/robots.js
@@ -1,29 +1,62 @@
 export const robots = [
-{
-    id:1,
-    name: 'Leanne Graham',
-    username: 'Bret',
-    email: 'Sincer@april.biz'
-},
-{
-
-    id: 2,
-    name: 'Ervin Howell',
-    username: 'Antonette',
-    email: 'Shanna@melissa.tv'
-},
-{
-
-    id: 3,
-    name: 'clementine Bauch',
-    username: 'Samantha',
-    email: 'Nathan@yesenia.net'
-},
-{
-
-    id: 4,
-    name: 'patricia Lebsack',
-    username: 'Karianne',
-    email: 'Julianne.oconner@Kory.org'
-},
-];
+    {
+     id: 1,
+     name: 'Azubuike Ebenizer',
+     username: 'Bret',
+     email: 'Sincere@april.biz'
+    },
+    {
+      id: 2,
+      name: 'Ervin Howell',
+      username: 'Antonette',
+      email: 'Shanna@nelissa.tv'
+    },
+    {
+        id: 3,
+        name: 'Clementine Bauch',
+        username: 'Sanantha',
+        email: 'Nathan@yesenia.net'
+    },
+    {
+        id: 4,
+        name: 'patricia Lobsack',
+        username: 'Karianne',
+        email: 'Julianne.oconner@kory.org'
+    },
+    {
+        id: 5,
+        name: 'Chelsey Dickson',
+        username: 'Kanren',
+        email: 'Lucio_Hettinger@annie.ca'
+    },
+    {
+        id: 6,
+        name: 'Mrs.felicity Dennis schulist',
+        username: 'Leopoldo_Corkery',
+        email: 'Karley_Dach@jasper.info' 
+    },
+    {
+        id: 7,
+        name: 'Kurtis weissnat',
+        username: 'Elwyn.skiles',
+        email: 'Telly.Hoeger@billy.biz'
+    },
+    {
+        id: 8,
+        name:'Nicholas Runolfsdottir v',
+        username: 'Maxime_Nienow',
+        email: 'Sherwood@rosamond.me'
+    },
+    {
+        id: 9,
+        name: 'Glenna Reichert',
+        username: 'Chaim Glenna',
+        email: 'Chaim_McDemott@dana.io'
+    },
+    {
+        id: 10,
+        name: 'Ebenco Reichert',
+        username: 'Glenna Rey',
+        email: 'Rey Padberge@Kanna.biz'
+    },
+]
